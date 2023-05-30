b9e82cf99d99499fc617e4de817ec54d99af5cc7
Derrick Tay <derricktay17@gmail.com>
2023-05-30 22:21:41 +0800

    fix:delete test

--- rpc-server/init.sql
+++ rpc-server/init.sql
@@ -1,8 +1,5 @@
--- CREATE USER 'test'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
--- GRANT ALL PRIVILEGES ON *.* TO 'username'@'localhost' WITH GRANT OPTION;
--- GRANT ALL PRIVILEGES ON *.* TO 'username'@'%'  WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
 
